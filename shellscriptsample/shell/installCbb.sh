8dfdbd40c7c6170c1e6211da358f53477f4191b7
donghyun <donghyun@donghyun-PC>
2015-06-15 19:27:52 +0900

    install script

---
+++ shellscriptsample/shell/installCbb.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+usage(){
+    echo "Usage: $0 filename"
+    exit 1
+}
+
+is_file_exits(){
+    local f="$1"
+    [[ -f "$f" ]] && return 0 || return 1
+}
+
+[[ $# -eq 0 ]] && usage
+
+TAR=$1
+
+DEL_HOME=$HOME/delivery
+TMP=$DEL_HOME/tmp
+
+cd $DEL_HOME
+
+if ( is_file_exits "$TAR" )
+then
+ echo "make tmp dir."
+ mkdir tmp
+ echo "Extract files"
+ tar -C tmp -xvzf $TAR
+else
+ echo "$TAR not found."
+ exit 1
+fi
+
+cd $CBP_HOME
+
+if ( is_file_exits "$TMP/app_home" )
+then
+ echo "del app_home"
+ rm -rf app_home
+ echo "install app_home"
+ mv $TMP/app_home ./
+ echo "app_home completed."
+else
+ echo "app_home skip"
+fi
+
+if (is_file_exits "$TMP/webui" )
+then
+ echo "del webui."
+ rm -rf webui
+ echo "install webui"
+ mv $TMP/webui ./
+ cd webui/server
+ ln -s ../../config/configjean.js config.js
+ echo "webui completed"
+else
+ echo "webui skip"
+fi
+
+cd $DEL_HOMEa
+ echo "del tmp"
+rm -rf tmp
+
+echo "end."
