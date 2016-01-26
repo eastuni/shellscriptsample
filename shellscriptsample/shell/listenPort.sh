89e2f12c83e651404bdb3c34eda3503d480726e3
eastuni <donghyuni@gmail.com>
2016-01-26 20:23:08 +0900

    linstenPort

---
+++ shellscriptsample/shell/listenPort.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+PORTLIST=(`netstat -tnlp|awk '{split($(4),a,":");print a[length(a)];}'`)
+PIDLIST=(`netstat -tnlp|awk '{split($(NF),a,"/");print a[1];}'`)
+echo $PORTLIST
+
+for i in "${!PIDLIST[@]}"
+do 
+  echo "=======    ${PIDLIST[$i]} ,  ${PORTLIST[$i]}  =============="
+  echo "ps -ef|grep ${PIDLIST[$i]}";
+  echo `ps -ef|grep ${PIDLIST[$i]}`;
+  echo
+done
+
